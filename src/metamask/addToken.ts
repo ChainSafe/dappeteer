789bcca92d63a3ee91e8cc98063fdde04f0095f6
Bernard <bero4net@gmail.com>
2022-01-12 16:40:34 +0100

    fix dappeteer to work with the latest active metamask version (1.8.1)

--- src/metamask/addToken.ts
+++ src/metamask/addToken.ts
@@ -3,11 +3,13 @@ import { Page } from 'puppeteer';
 export const addToken = (page: Page) => async (tokenAddress: string): Promise<void> => {
   await page.bringToFront();
 
-  const addTokenButton = await page.waitForSelector('.add-token-button > button');
+  const addTokenButton = await page.waitForSelector(
+    '.box.import-token-link.box--flex-direction-row.box--text-align-center > a',
+  );
   await addTokenButton.click();
 
   const addressInput = await page.waitForSelector('#custom-address');
-  addressInput.type(tokenAddress);
+  await addressInput.type(tokenAddress);
 
   await page.waitForTimeout(4000);
 
