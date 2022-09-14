1c7557969f39f02e10fe1814092757186e337f1d
Bernard <bero4net@gmail.com>
2022-09-14 14:04:58 +0200

    fix jest config issue

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 export { getMetamask, getMetamaskWindow } from './metamask';
 export * from './types';
 export * from './setup';
+export { DappateerJestConfig } from './jest/global';
 
 // default constants
 export const RECOMMENDED_METAMASK_VERSION = 'v10.15.0';
--- src/types.ts
+++ src/types.ts
@@ -6,12 +6,14 @@ import { RECOMMENDED_METAMASK_VERSION } from './index';
 
 export type LaunchOptions = OfficialOptions | CustomOptions;
 
-export type OfficialOptions = BrowserLaunchArgumentOptions & {
+type DappaterBrowserLaunchArgumentOptions = Omit<BrowserLaunchArgumentOptions, 'headless'>;
+
+export type OfficialOptions = DappaterBrowserLaunchArgumentOptions & {
   metamaskVersion: typeof RECOMMENDED_METAMASK_VERSION | 'latest' | string;
   metamaskLocation?: Path;
 };
 
-export type CustomOptions = BrowserLaunchArgumentOptions & {
+export type CustomOptions = DappaterBrowserLaunchArgumentOptions & {
   metamaskVersion?: string;
   metamaskPath: string;
 };
