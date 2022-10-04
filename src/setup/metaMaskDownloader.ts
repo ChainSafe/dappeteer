314dd68ee089e0f422715ef83ffc20b9fac8a059
Marin Petrunic <marin.petrunic@gmail.com>
2022-10-04 17:12:08 +0200

    address PR comments

--- src/setup/metaMaskDownloader.ts
+++ src/setup/metaMaskDownloader.ts
@@ -1,6 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access */
 import * as fs from "fs";
 import { IncomingMessage } from "http";
 import { get } from "https";
