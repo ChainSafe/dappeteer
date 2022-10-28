3cd36b08c8f75e5da4f7a0b8cf61d7a0e19ab5df
Bernard <bero4net@gmail.com>
2022-10-28 17:26:28 +0200

    add methods snap

---
+++ test/flask/methods-snap/src/index.ts
@@ -0,0 +1,21 @@
+import { OnRpcRequestHandler } from "@metamask/snap-types";
+
+export const onRpcRequest: OnRpcRequestHandler = ({ origin, request }) => {
+  switch (request.method) {
+    case "confirm":
+      return wallet.request({
+        method: "snap_confirm",
+        params: [
+          {
+            prompt: `Hello, ${origin}!`,
+            description:
+              "This custom confirmation is just for display purposes.",
+            textAreaContent:
+              "But you can edit the snap source code to make it do something, if you want to!",
+          },
+        ],
+      });
+    default:
+      throw new Error("Method not found.");
+  }
+};
