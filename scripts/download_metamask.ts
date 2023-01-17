f6bab41abf2f9a803ffe3212bb7930fabf869ddb
Marin Petrunic <marin.petrunic@gmail.com>
2023-01-17 13:03:03 +0100

    address PR comments

--- scripts/download_metamask.ts
+++ scripts/download_metamask.ts
@@ -1,13 +1,11 @@
-import downloader, {defaultDirectory} from "../src/setup/utils/metaMaskDownloader";
+import downloader from "../src/setup/utils/metaMaskDownloader";
 import {RECOMMENDED_METAMASK_VERSION} from "../src"
 
 (async function() {
     await downloader(RECOMMENDED_METAMASK_VERSION, {
-        location: defaultDirectory,
         flask: false,
       });
     await downloader(RECOMMENDED_METAMASK_VERSION, {
-        location: defaultDirectory,
         flask: true,
       });
 })()
